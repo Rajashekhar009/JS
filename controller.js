6d12fbe9bb439f5030fc3f1286dc52dd5143d4a9
Rajashekhar Anumalla <shekaranumalla@gmail.com>
2018-08-11 03:14:29 +0200

    JS and Design completed
    JS and Design completed

--- controller.js
+++ controller.js
@@ -1,1 +1,20 @@
 //JS Controller file
+function funTopNavMenu(strID, strP) {
+	
+	var strTtl = document.getElementById(strID).innerHTML;
+    var divItemDtls = document.getElementById("divItemDtls");
+	var SubCatItem = document.getElementById("SubCatItem");
+	var SubCatItemDesc = document.getElementById("SubCatItemDesc");
+	
+	if(strP!="Home"){
+				
+		divItemDtls.style.display="block";
+		
+		SubCatItem.innerHTML = strTtl;
+		
+	}	
+	else{
+		divSubCat.style.display="none";
+	}
+	
+}
